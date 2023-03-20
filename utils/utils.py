e544c3621e040db7c28dd794160a11b759a4796e
ZhaoBin-PAN <68942564+yuzhaoOvO@users.noreply.github.com>
2023-03-20 14:55:11 +0800

    工具类

---
+++ utils/utils.py
@@ -0,0 +1,28 @@
+
+import torch
+import yaml
+from PIL import Image
+from torch.nn.functional import one_hot
+
+
+def load_config_util(config_path):
+    config_file = open(config_path, 'r', encoding='utf-8')
+    config_data = yaml.load(config_file)
+    return config_data
+
+
+def keep_shape_resize(frame, size=224):   # 把图片放缩为224*224
+    w, h = frame.size
+    temp = max(w, h)
+    mask = Image.new('RGB', (temp, temp), (0, 0, 0))
+    if w >= h:
+        position = (0, (w - h) // 2)
+    else:
+        position = ((h - w) // 2, 0)
+    mask.paste(frame, position)
+    mask = mask.resize((size, size))
+    return mask
+
+
+def label_one_hot(label):
+    return one_hot(torch.tensor(label))
